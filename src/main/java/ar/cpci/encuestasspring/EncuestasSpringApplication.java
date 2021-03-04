67f0e0c4dda7cd75bc13b45bc8f9500354fa9391
Ezequiel Sosa <ezequieljsosa@gmail.com>
2021-03-04 17:54:43 -0300

    pre clase 04032021

--- src/main/java/ar/cpci/encuestasspring/EncuestasSpringApplication.java
+++ src/main/java/ar/cpci/encuestasspring/EncuestasSpringApplication.java
@@ -14,6 +14,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -79,6 +80,11 @@ public class EncuestasSpringApplication implements WebMvcConfigurer {
         return bean;
     }
 
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/logout").setViewName("logout");
+        registry.addViewController("/login").setViewName("login");
+    }
 
 
 }
